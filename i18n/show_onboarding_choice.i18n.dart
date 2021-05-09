edb1ec1f60e338e66b1ffa095056ab54aec94d94
andarianlb <amir.yaari@gmail.com>
2021-05-09 20:01:17 +0300

    Update show_onboarding_choice.i18n.dart

--- i18n/show_onboarding_choice.i18n.dart
+++ i18n/show_onboarding_choice.i18n.dart
@@ -4,16 +4,16 @@ extension Localization on String {
 
 static var _t = Translations.byLocale("en_us") +
     {
-      "es_es": {
-        'You can ask for an invite at': "Puedes solicitar una invitación en",
-        "Membership based on Web of Trust": "Membresía basada en una Red de Confianza",
-        "By signing up, you agree to our terms and privacy policy": "Al registrarte, aceptas nuestros términos y política de privacidad",
-        'Terms & Conditions': "Términos y condiciones",
-        'Privacy Policy': "Política de privacidad",
-        'If you have an account\nclick here': "Si tienes cuenta\ntoca aquí",
-        'Import private key': "Importar llave privada",
-        'If you have an invite\nclick here': "Si tienes invitación\ntoca aquí",
-        "Claim invite code": "Reclama código de invitación",
+      "he_il": {
+        'You can ask for an invite at': "אפשר לבקש הזמנה ב",
+        "Membership based on Web of Trust": "חברות מבוססת על רשת אמון",
+        "By signing up, you agree to our terms and privacy policy": "בהרשמתך, הסכמת לתנאים ולמדיניות הפרטיות שלנו",
+        'Terms & Conditions': "תנאי שימוש",
+        'Privacy Policy': "מדיניות הפרטיות",
+        'If you have an account\nclick here': "אם יש לכם חשבון\nהקלק/י כאן",
+        'Import private key': "יבא ססמא פרטית",
+        'If you have an invite\nclick here': "אם יש לכם הזמנה\nהקלק/י כאן aquí",
+        "Claim invite code": "בקש דוד הזמנה",
       }
     };
 
