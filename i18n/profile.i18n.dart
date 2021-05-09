ca6ea9ddaea12702b56c6723670ea038b1710a80
andarianlb <amir.yaari@gmail.com>
2021-05-09 15:21:19 +0300

    Update profile.i18n.dart

--- i18n/profile.i18n.dart
+++ i18n/profile.i18n.dart
@@ -3,38 +3,38 @@ import 'package:i18n_extension/i18n_extension.dart';
 extension Localization on String {
   static var _t = Translations.byLocale("en_us") +
       {
-        "es_es": {
-          'Full Name': "Nombre completo",
-          'Enter your name': "Ingresa tu nombre",
-          "Name cannot be empty": "Nombre no puede estar vacío",
-          'Save': "Guardar",
-          "Terms & Conditions": "Términos y condiciones",
-          'Privacy Policy': "Política de privacidad",
-          'Export private key': "Exportar llave privada",
-          'Logout': "Cerrar sesión",
-          'Choose Picture': "Escoger foto",
-          'Take a picture': "Tomar foto",
-          'Profile updated successfully.': "Perfil actualizado con éxito",
+        "he_il": {
+          'Full Name': "שם מלא",
+          'Enter your name': "הכניסו את שמכם",
+          "Name cannot be empty": "השם לא יכול להיות ריק",
+          'Save': "שמירה",
+          "Terms & Conditions": "תנאים והסכמים",
+          'Privacy Policy': "מדיניות פרטיות",
+          'Export private key': "ייצאו מפתח פרטי",
+          'Logout': "להתנתק",
+          'Choose Picture': "בחירת תמונה",
+          'Take a picture': "צלמו תמונה",
+          'Profile updated successfully.': "הפרופיל עודכן בהצלחה",
           'An error occured, please try again.':
-              "Ocurrió un error, por favor intentar de nuevo",
+              "נמצאה שגיאה, אנא נסו שוב",
           'Save private key in secure place - to be able to restore access to your wallet later':
-              "Guarda la llave privada en un lugar seguro - para poder recuperar acceso a tu billetera luego",
-          "Save private key": "Guarda la llave privada",
-          "Key Guardians": "Guardianes de Llave",
+              "שמרו את המפתח הפרטי במקום מאובטח - כדי שתוכלו לשחזר גישה לארנק שלכם מאוחר יותר",
+          "Save private key": "שמירת מפתח פרטי",
+          "Key Guardians": "שומרי מפתח",
           // profile_v2
-          "Contribution Score": "Puntaje de contribución",
-          "Badges Earned": "Insignias ganadas",
-          "Bioregion": "Bioregion",
-          "Currency": "Moneda",
-          "Skills & Interest": "Habilidades e interés",
-          "You are on the way from": "Estás en el camino de",
-          "Resident": "Residente",
-          "to": "a",
-          "Citizen": "Ciudadano",
-          "View your progress": "Ver tu progreso",
-          "Settings": "Configuraciones",
-          "Security": "Seguridad",
-          "Support": "Soporte"
+          "Contribution Score": "נקודות תרומה",
+          "Badges Earned": "אותות שהרווחת",
+          "Bioregion": "אזור",
+          "Currency": "מטבע",
+          "Skills & Interest": "כישורים ותחביבים",
+          "You are on the way from": "אתם על הדרך מ",
+          "Resident": "תושב",
+          "to": "ל",
+          "Citizen": "אזרח",
+          "View your progress": "ההתקדמות שלכם",
+          "Settings": "הגדרות",
+          "Security": "אבטחה",
+          "Support": "תמיכה"
         }
       };
 
