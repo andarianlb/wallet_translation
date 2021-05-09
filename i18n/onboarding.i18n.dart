19dac7a35bdbbeb0bda52116696ad184534b4df0
andarianlb <amir.yaari@gmail.com>
2021-05-09 15:24:04 +0300

    Update onboarding.i18n.dart

--- i18n/onboarding.i18n.dart
+++ i18n/onboarding.i18n.dart
@@ -4,22 +4,22 @@ extension Localization on String {
 
 static var _t = Translations.byLocale("en_us") +
     {
-      "es_es": {
+      "he_il": {
         
         // TRANSLATION NOTE: These are automatic translations by Google Translate - please replace
 
-        'Better than free transactions': 'Transacciones mejor que gratis',
+        'Better than free transactions': 'טוב יותר מהעברות חינמיות',
         "Make payments globally without any fees. Earn rewards when you support 'regenerative' organisations and people.":
-        "Realiza pagos a nivel mundial sin ningún cargo. Obtenga recompensas cuando apoye a organizaciones y personas 'regenerativas'.",
+        "שלמו בכל העולם ללא עמלות. הרוויחו פרסים בעבור תמיכה בארגונים 'מתחדשים' ובאנשים",
         'Vote directly on social and environmental impact projects you care about.':
-        'Vota directamente sobre los proyectos de impacto social y ambiental que te interesan.',
-        'Citizen Campaigns': 'Campañas ciudadanas',
+        'הצבעו באופן ישיר על פרויקטים עם השפעה סביבתית וחברתית שאכפת לכם ממנה',
+        'Citizen Campaigns': 'קמפיינים אזרחיים',
         'Unite with a global movement of organisations and people to regenerate our planet and heal our economy.': 
-        'Únete a un movimiento global de organizaciones y personas para regenerar nuestro planeta y sanar nuestra economía.',
-        'Regenerative Economy': 'Economía regenerativa',
-        'NEXT': 'SIGUIENTE',
-        'BACK': 'ATRÁS',
-        'JOIN NOW': 'ÚNETE AHORA',
+        'הצטרפו לתנועה עולמית של ארגונים ואנשים לריפוי הכוכב והכלכלה שלנו',
+        'Regenerative Economy': 'כלכלה מתחדשת',
+        'NEXT': 'הבא',
+        'BACK': 'הקודם',
+        'JOIN NOW': 'הצטרפו עכשיו',
       }
     };
 
