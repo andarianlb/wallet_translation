4d334cf6beff625c7720c13b6f23a3a343de5589
andarianlb <amir.yaari@gmail.com>
2021-05-09 20:08:56 +0300

    Update welcome.i18n.dart

--- i18n/welcome.i18n.dart
+++ i18n/welcome.i18n.dart
@@ -4,10 +4,10 @@ extension Localization on String {
 
 static var _t = Translations.byLocale("en_us") +
     {
-      "es_es": {
-      'Your wallet almost ready - choose passcode to finish setup': "Tu billetera está casi lista - escoge un código de seguridad para finalizar",
-      'Welcome, %s': "Bienvenido, %s",
-      "FINISH": "FINALIZAR",
+      "he_il": {
+      'Your wallet almost ready - choose passcode to finish setup': "הארנק שלכם כמעט מוכן - ביחרו סיסמא כדי לסיים הגדרות",
+      'Welcome, %s': "ברוכים הבאים, %s",
+      "FINISH": "סיום",
       
 
       }
